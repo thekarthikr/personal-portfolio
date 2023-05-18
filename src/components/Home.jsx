83f154bf39e2d3dcbf41576073ef1c4e1a1b55c7
Karthik <karthikwebdev@outlook.com>
2023-05-18 16:12:21 +0530

    Build Hero Section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-
-const Home = () => {
-  return <div>Home</div>;
-};
+const Home = () => (
+  <main id='Home' className='hero section'>
+    <div className='hero__content'>
+      <h1 className='hero__heading'>
+        Helping nice <br />
+        people
+        <span className='text-gradient'>build</span> <br />
+        nice products
+      </h1>
+      <p className='hero__sub'>Building Better Web Experiences</p>
+    </div>
+  </main>
+);
 
 export default Home;
